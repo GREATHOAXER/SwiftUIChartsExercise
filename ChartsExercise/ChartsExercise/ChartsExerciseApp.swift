e7427190426801fd2afb20e3de7e49fbc006dbff
GREATHOAXER <shorelinesquare@naver.com>
2023-01-21 16:04:19 +0900

    :tada: initial project
    initial commit

---
+++ ChartsExercise/ChartsExercise/ChartsExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsExerciseApp.swift
+//  ChartsExercise
+//
+//  Created by Hyung Seo Han on 2023/01/21.
+//
+
+import SwiftUI
+
+@main
+struct ChartsExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
